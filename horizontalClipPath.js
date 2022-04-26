9b2b687d5e85dfd6c2143b22aad0201ae53518ce
Zieleniowy <m.rynkowski@zhr.pl>
2022-04-26 10:29:11 +0200

    making a component module type

--- horizontalClipPath.js
+++ horizontalClipPath.js
@@ -1,12 +1,19 @@
-export default (thickness) => (n, count, active) => {
-  const l = (1 / count) * 100;
-  if (active === null)
-    return `polygon(${n * l}% 0, ${n * l + l}% 0, ${n * l + l}% 100%, ${n * l}% 100%)`;
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _default = thickness => (n, count, active) => {
+  const l = 1 / count * 100;
+  if (active === null) return `polygon(${n * l}% 0, ${n * l + l}% 0, ${n * l + l}% 100%, ${n * l}% 100%)`;
+
   if (active === n) {
     return `polygon(${n * thickness}% 0, ${100 - (count - n - 1) * thickness}% 0, ${100 - (count - n - 1) * thickness}% 100%, ${n * thickness}% 100%)`;
   } else {
-    return active < n
-      ? `polygon(${100 - (count - n) * thickness}% 0, ${100 - (count - n - 1) * thickness}% 0, ${100 - (count - n - 1) * thickness}% 100%, ${100 - (count - n) * thickness}% 100%)`
-      : `polygon(${n * thickness}% 0, ${n * thickness + thickness}% 0, ${n * thickness + thickness}% 100%, ${n * thickness}% 100%)`;
+    return active < n ? `polygon(${100 - (count - n) * thickness}% 0, ${100 - (count - n - 1) * thickness}% 0, ${100 - (count - n - 1) * thickness}% 100%, ${100 - (count - n) * thickness}% 100%)` : `polygon(${n * thickness}% 0, ${n * thickness + thickness}% 0, ${n * thickness + thickness}% 100%, ${n * thickness}% 100%)`;
   }
 };
+
+exports.default = _default;
--- verticalClipPath.js
+++ verticalClipPath.js
@@ -1,13 +1,19 @@
-export default (thickness) => (n, count, active) => {
-    const l = (1 / count) * 100;
-    if (active === null)
-      return `polygon(0 ${n * l}%, 0 ${n * l + l}%, 100% ${n * l + l}%, 100% ${n * l}%)`;
-    if (active === n) {
-      return `polygon(0 ${n * thickness}%, 0 ${100 - (count - n - 1) * thickness}%, 100% ${100 - (count - n - 1) * thickness}%, 100% ${n * thickness}%)`;
-    } else {
-      return active < n
-        ? `polygon(0 ${100 - (count - n) * thickness}%, 0 ${100 - (count - n - 1) * thickness}%, 100% ${100 - (count - n - 1) * thickness}%, 100% ${100 - (count - n) * thickness}%)`
-        : `polygon(0 ${n * thickness}%, 0 ${n * thickness + thickness}%, 100% ${n * thickness + thickness}%, 100% ${n * thickness}%)`;
-    }
-  };
-  
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _default = thickness => (n, count, active) => {
+  const l = 1 / count * 100;
+  if (active === null) return `polygon(0 ${n * l}%, 0 ${n * l + l}%, 100% ${n * l + l}%, 100% ${n * l}%)`;
+
+  if (active === n) {
+    return `polygon(0 ${n * thickness}%, 0 ${100 - (count - n - 1) * thickness}%, 100% ${100 - (count - n - 1) * thickness}%, 100% ${n * thickness}%)`;
+  } else {
+    return active < n ? `polygon(0 ${100 - (count - n) * thickness}%, 0 ${100 - (count - n - 1) * thickness}%, 100% ${100 - (count - n - 1) * thickness}%, 100% ${100 - (count - n) * thickness}%)` : `polygon(0 ${n * thickness}%, 0 ${n * thickness + thickness}%, 100% ${n * thickness + thickness}%, 100% ${n * thickness}%)`;
+  }
+};
+
+exports.default = _default;
